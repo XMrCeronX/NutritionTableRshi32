9b06e808951c70db8a7b08783067922f18542bab
Artyom <artemkakrg@gmail.com>
2025-04-02 23:32:40 +0300

    добавил DEFAULT_DATE_FORMAT и убрал статичные методы

--- FileNameGenerator.py
+++ FileNameGenerator.py
@@ -2,9 +2,9 @@ from datetime import datetime, timedelta
 
 
 class FileNameGenerator:
+    DEFAULT_DATE_FORMAT = '%d.%m.%Y'
 
-    @staticmethod
-    def get_current_week(include_days=None):
+    def get_current_week(self, include_days=None):
         """
         Возвращает список дат текущей недели.
 
@@ -14,25 +14,18 @@ class FileNameGenerator:
         """
         if include_days is None:
             include_days = [0, 1, 2, 3, 4, 5, 6]  # Включить все дни недели
-
-        # Получаем текущую дату
         today = datetime.now()
-
         # Находим первый день недели (понедельник)
         start_of_week = today - timedelta(days=today.weekday())
-
-        # Создаем список дат
         week_dates = []
-
         for i in range(7):  # Проходим по всем дням недели
             current_day = start_of_week + timedelta(days=i)
             if current_day.weekday() in include_days:
-                week_dates.append(current_day.strftime('%d.%m.%Y'))
+                week_dates.append(current_day.strftime(self.DEFAULT_DATE_FORMAT))
 
         return week_dates
 
-    @staticmethod
-    def get_date_range(start_date=None, end_date=None, include_days=None):
+    def get_date_range(self, start_date=None, end_date=None, include_days=None):
         """
         Возвращает список дат в указанном диапазоне.
 
@@ -43,29 +36,23 @@ class FileNameGenerator:
         :return: Список строк с датами в формате 'дд.мм.гггг'.
         """
         if start_date is None:
-            start_date = datetime.now().strftime('%d.%m.%Y')
+            start_date = datetime.now().strftime(self.DEFAULT_DATE_FORMAT)
 
         if end_date is None:
-            end_date = (datetime.now() + timedelta(days=7)).strftime('%d.%m.%Y')
+            end_date = (datetime.now() + timedelta(days=7)).strftime(self.DEFAULT_DATE_FORMAT)
 
         if include_days is None:
             include_days = [0, 1, 2, 3, 4, 5, 6]  # Включить все дни недели
 
-        # Преобразуем даты в datetime объекты
-        start_date_obj = datetime.strptime(start_date, '%d.%m.%Y')
-        end_date_obj = datetime.strptime(end_date, '%d.%m.%Y')
+        start_date_obj = datetime.strptime(start_date, self.DEFAULT_DATE_FORMAT)
+        end_date_obj = datetime.strptime(end_date, self.DEFAULT_DATE_FORMAT)
 
-        # Создаем список дат
         date_range = []
 
         current_date = start_date_obj
         while current_date <= end_date_obj:
             if current_date.weekday() in include_days:
-                date_range.append(current_date.strftime('%d.%m.%Y'))
+                date_range.append(current_date.strftime(self.DEFAULT_DATE_FORMAT))
             current_date += timedelta(days=1)
 
         return date_range
-
-    # # Пример использования
-    # current_week_dates = get_current_week(include_days=[0, 1, 2, 3])  # Включить понедельник по четверг
-    # print(current_week_dates)
