f667ce538b0f4bd0c09828499f8540bbf582b8be
Artyom <artemkakrg@gmail.com>
2025-04-02 23:46:49 +0300

    добавил декоратор для HTTP ошибок

---
+++ decorators/http_errors.py
@@ -0,0 +1,15 @@
+from googleapiclient.errors import HttpError
+
+
+def handle_http_errors(exception_type=HttpError, error_message="HttpError"):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            try:
+                return func(*args, **kwargs)
+            except exception_type as error:
+                print(f"{error_message}: {error}.")
+                return None  # обязателен
+
+        return wrapper
+
+    return decorator
